fc8ee78a90430862b7dfde6ba69ae7bfb4ab4674
Duke <amiscary@gmail.com>
2016-10-10 08:44:13 +0300

    fixed font on large screens

--- src/contacts.php
+++ src/contacts.php
@@ -130,35 +130,35 @@
 </html>
 
 <?php
-if (isset($_GET['submit'])) {
+// if (isset($_GET['submit'])) {
     // echo "form submit";
-}
-/* Осуществляем проверку вводимых данных и их защиту от враждебных
-скриптов */
-$name = htmlspecialchars($_POST["name"]);
-$company = htmlspecialchars($_POST["company"]);
-$event = htmlspecialchars($_POST["event"]);
-$interesting = htmlspecialchars($_POST["interesting"]);
-$informfrom = htmlspecialchars($_POST["informfrom"]);
-$phone = htmlspecialchars($_POST["phone"]);
-$mail = htmlspecialchars($_POST["mail"]);
-/* Устанавливаем e-mail адресата */
-$myemail = "kompot.prod@ya.ru";
-/* Проверяем заполнены ли обязательные поля ввода, используя check_input
-функцию */
-// $your_name = check_input($_POST["your_name"], "Введите ваше имя!");
-// $tema = check_input($_POST["tema"], "Укажите тему сообщения!");
-// $email = check_input($_POST["email"], "Введите ваш e-mail!");
-// $message = check_input($_POST["message"], "Вы забыли написать сообщение!");
-/* Проверяем правильно ли записан e-mail */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $mail))
-{
-show_error("<br /> Е-mail адрес не существует");
-}
-/* Создаем новую переменную, присвоив ей значение */
-$message_to_myemail = "Привет! Меня зовут $name, я представляю компанию $company, или пишу вам лично от себя, потому что планирую организовать $event. Меня заинтересовали ваши проекты особенно $name. Я узнал о вас, благодаря $informfrom. Свяжитесь со мной, пожалуйста,
-по номеру $phone, или напишите мне на почтовый ящик $mail. Жду с нетерпением!";
-/* Отправляем сообщение, используя mail() функцию */
-$from  = "From: $name <$mail> \r\n Reply-To: $mail \r\n";
-mail($myemail, "Заявка", $message_to_myemail, $from);
+    /* Осуществляем проверку вводимых данных и их защиту от враждебных
+    скриптов */
+    $name = htmlspecialchars($_POST["name"]);
+    $company = htmlspecialchars($_POST["company"]);
+    $event = htmlspecialchars($_POST["event"]);
+    $interesting = htmlspecialchars($_POST["interesting"]);
+    $informfrom = htmlspecialchars($_POST["informfrom"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $mail = htmlspecialchars($_POST["mail"]);
+    /* Устанавливаем e-mail адресата */
+    $myemail = "kompot.prod@ya.ru";
+    /* Проверяем заполнены ли обязательные поля ввода, используя check_input
+    функцию */
+    // $your_name = check_input($_POST["your_name"], "Введите ваше имя!");
+    // $tema = check_input($_POST["tema"], "Укажите тему сообщения!");
+    // $email = check_input($_POST["email"], "Введите ваш e-mail!");
+    // $message = check_input($_POST["message"], "Вы забыли написать сообщение!");
+    /* Проверяем правильно ли записан e-mail */
+    // if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $mail))
+    // {
+    // show_error("<br /> Е-mail адрес не существует");
+    // }
+    /* Создаем новую переменную, присвоив ей значение */
+    $message_to_myemail = "Привет! Меня зовут $name, я представляю компанию $company, или пишу вам лично от себя, потому что планирую организовать $event. Меня заинтересовали ваши проекты особенно $name. Я узнал о вас, благодаря $informfrom. Свяжитесь со мной, пожалуйста,
+    по номеру $phone, или напишите мне на почтовый ящик $mail. Жду с нетерпением!";
+    /* Отправляем сообщение, используя mail() функцию */
+    // $from  = "From: $name <$mail> \r\n Reply-To: $mail";
+    mail($myemail, "Заявка", $message_to_myemail, "From: a74026@lovesosna.mcdir.ru");
+// }
 ?>
