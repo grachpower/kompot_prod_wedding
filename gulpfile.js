00d9d906341c291159d9a01c78f63c19ce6c067d
Artyom Andreev <amiscary@gmail.com>
2018-04-15 20:16:32 +0300

    add images

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,87 @@
 const gulp = require('gulp');
 const cssnano = require('gulp-cssnano');
 const del = require('del');
-const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-html-minifier');
 const watch = require('gulp-watch');
 const fileinclude = require('gulp-file-include');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const spritesmith = require('gulp.spritesmith');
+const imageop = require('gulp-image-optimization');
+
 
 gulp.task('sprite', function () {
-  return gulp.src('img/icons/*.png')
-    .pipe(spritesmith({
-      imgName: 'icons.png'
-    }));
+    return gulp.src('img/icons/*.png')
+        .pipe(spritesmith({
+            imgName: 'icons.png'
+        }));
 });
 
-gulp.task('fileinclude', function() {
-  return gulp.src(['index.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./'));
+gulp.task('fileinclude', function () {
+    return gulp.src(['index.html'])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('sass', function () {
-  return gulp.src('src/css/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'));
+    return gulp.src('src/css/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'));
 });
 
 gulp.task('sass-watch', function () {
-  return watch('src/css/**/*.scss', function () {
-    gulp.src('src/css/style.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      }))
-      .pipe(gulp.dest('src/css'));
-  });
+    return watch('src/css/**/*.scss', function () {
+        gulp.src('src/css/style.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('src/css'));
+    });
 });
 
-gulp.task('css-min', function() {
-  return gulp.src('src/css/style.css')
-    .pipe(cssnano())
-    .pipe(gulp.dest('dist/css'));
+gulp.task('css-min', function () {
+    return gulp.src('src/css/style.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('autoprefix', function() {
-  return gulp.src('src/css/style.css')
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('src/css/'));
+gulp.task('autoprefix', function () {
+    return gulp.src('src/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('src/css/'));
 });
 
-gulp.task('htmlmin', function() {
-  return gulp.src('src/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(gulp.dest('dist/'));
+gulp.task('htmlmin', function () {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('img', function() {
-  return gulp.src('src/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img/'));
+gulp.task('img', function () {
+    return gulp.src(['src/**/*.png','src/**/*.jpg','src/**/*.gif','src/**/*.jpeg'])
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/img/'));
 });
 
-gulp.task('clean', function() {
-  return del.sync('dist');
+gulp.task('clean', function () {
+    return del.sync('dist');
 });
 
-gulp.task('build', ['clean', 'sass', 'autoprefix', 'img', 'htmlmin', 'css-min'], function() {
+gulp.task('build', ['clean', 'sass', 'autoprefix', 'img', 'htmlmin', 'css-min'], function () {
     gulp.src('src/font-awesome/**/*')
         .pipe(gulp.dest('dist/font-awesome/'));
     gulp.src('src/fonts/**/*')
